b00a368950bda1dcb53d509c046d91749bcf9dbd
x-m-l <ll>
2018-07-06 17:17:13 +0800

    收银台支付

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,5 +18,15 @@ Page({
     this.setData({
       personId: wx.getStorageSync('personId')
     })
+  },
+  goBaidu: function () {
+    wx.navigateTo({
+      url: '../q/q', //
+      success: function () {
+
+      },       //成功后的回调；
+      fail: function () { },         //失败后的回调；
+      complete: function () { }      //结束后的回调(成功，失败都会执行)
+    })
   }
 })
